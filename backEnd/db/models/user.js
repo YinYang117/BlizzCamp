6cc372b55e0d733f136db09f72a49f9743e70407
YinYang117 <slcryan2010@yahoo.com>
2022-03-03 16:42:26 -0700

    created static User methods and used scope

--- backEnd/db/models/user.js
+++ backEnd/db/models/user.js
@@ -1,10 +1,55 @@
 'use strict';
 const { Validator } = require('sequelize');
+const bcrypt = require('bcryptjs');
+
+// TODO
+// what scope do these need to be nested in
+User.getCurrentUserById = async function (id) {
+  return await User.scope('currentUser').findByPk(id);
+};
+
+User.prototype.toSafeObject = function() { // this cannot be an arrow function
+  const { id, username, email } = this; // context will be the User instance
+  return { id, username, email };
+};
+
+User.prototype.validatePassword = function (password) {
+   return bcrypt.compareSync(password, this.hashedPassword.toString())
+}
+
+User.login = async function ({ credential, password }) {
+// credential could be email or username as both are unique
+// Should allow user to enter either-or to login
+  const { Op } = require('sequelize');
+  const user = await User.scope('loginUser').findOne({
+    where: {
+      [Op.or]: {
+        username: credential,
+        email: credential
+      }
+    }
+  });
+
+  if (user && user.validatePassword(password)) {
+    return await User.scope('currentUser').findByPk(user.id)
+  }
+}
+
+User.signup = async function ({ username, email, password }) {
+  const hashedPassword = bcrypt.hashSync(password);
+  const user = await User.create({
+    username,
+    email,
+    hashedPassword
+  });
+  return await User.scope('currentUser').findByPk(user.id);
+};
 
 module.exports = (sequelize, DataTypes) => {
-  // Some things, like unique, could be defined here and in migrations
-  // It could be considered very explicit, but also redundant
+  // Some attributes, like unique, could be defined here and in migrations
+  // It'd explicit, also redundant
   // I think allow null is ^, but is important enough to reiterate
+
   const User = sequelize.define('User', {
     username: {
       type: DataTypes.STRING,
@@ -42,9 +87,11 @@ module.exports = (sequelize, DataTypes) => {
       scopes: {
         currentUser: {
           attributes: { exclude: ['hashedPassword']}
+          // returns a user obj without the hashedpass making it to the frontend
         },
         loginUser: {
           attributes: {}
+          // sets aside emtpy {} to fill in with our login credentials
         }
       }
     });
