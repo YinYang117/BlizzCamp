3d3e5605dc00dee901e0fe9f86acc7dd819f3ee8
YinYang117 <slcryan2010@yahoo.com>
2022-03-03 13:49:39 -0700

    two more error handlers

--- backEnd/app.js
+++ backEnd/app.js
@@ -7,11 +7,16 @@ const cookieParser = require('cookie-parser');
 const { enviroment } = require('./config');
 const isProduction = enviroment === 'production';
 const routes = require('./routes');
+const {ValidationError} =require('sequelize');
+
 
 const app = express();
 app.use(morgan('dev')); // logs information about reqs and res's
 app.use(cookieParser());
 app.use(express.json());
+
+/////////////////////////////////////////////////////
+// Route protection / security
 
 if (!isProduction) {
   // Use in production because React front is different server
@@ -39,7 +44,41 @@ app.use(
 
 app.use(routes);
 
+// Route connections finished
+/////////////////////////////////////////////////////
 
+// 404 Error
+app.use((req, res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource not found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+  // ^ passing err into next allows all other error
+  // handlers after this to still be invoked with their next's
+});
+
+// Sequelize errors
+app.use((err, req, res, next) => {
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map(e => e.message);
+    err.title = 'Validation error';
+  }
+  next(err)
+})
+
+// Error formatter
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+    // Ternary operator  a condition, ?, truthy outcome, :, falsey outcome
+  })
+})
 
 
 
