51d13115c201412d6f4f70a7e97f32e5d86de81b
YinYang117 <slcryan2010@yahoo.com>
2022-03-03 17:11:54 -0700

    restore user and require auth

--- backEnd/utils/auth.js
+++ backEnd/utils/auth.js
@@ -20,8 +20,48 @@ const setTokenCookie = (res, user) => {
     maxAge: expiresIn * 1000, // maxAge is milliseconds
     httpOnly: true,
     secure: isProduction,
-    sameSite: isProduction && "Lax" 
+    sameSite: isProduction && "Lax"
   });
 
   return token;
-}
+};
+
+// pre-middleware and follow auth middleware
+const restoreUser = (req, res, next) => {
+  // token parsed from cookies
+  const { token } = req.cookies;
+
+  return jwt.verify(token, secret, null, async (err, jwtPayload) => {
+    if (err) return next();
+
+    try {
+      const { id } = jwtPayload.data;
+      req.user = await User.scope('currentUser').findByPk(id);
+    } catch (e) {
+      res.clearCookie('token');
+      return next();
+    }
+
+    if (!req.user) res.clearCookie('token');
+
+    return next();
+  });
+};
+
+// pre-middleware and follow auth middleware
+// If no current user, return an error
+const requireAuth = [
+  restoreUser,
+
+  function (req, res, next) {
+    if (req.user) return next();
+
+    const err = new Error('Unauthorized');
+    err.title = 'Unauthorized';
+    err.errors = ['Unauthorized'];
+    err.status = 401;
+    return next(err);
+  }
+];
+
+module.exports = { setTokenCookie, restoreUser, requireAuth };
