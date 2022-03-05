49f28d2bb4bacf9672ae9023bdea6de1ae672115
YinYang117 <slcryan2010@yahoo.com>
2022-03-04 21:06:59 -0700

    staring on navigation

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -3,5 +3,13 @@ import * as sessionActions from '../../store/session';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 
+function navigation() {
+
+  return (
+    <ul>
+      {}
+    </ul>
+  )
+}
 // show a button or link or something that onclick useEffect:
 // dispatch(sessionActions.logout)
