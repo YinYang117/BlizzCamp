139b90b790b6073edb40cec4645f9a2184220118
YinYang117 <slcryan2010@yahoo.com>
2022-03-05 14:06:07 -0700

    finished the Navigation rendering

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,15 +1,30 @@
-import React, { useState } from "react";
-import * as sessionActions from '../../store/session';
-import { useDispatch, useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import ProfileButton from './ProfileButton';
+import './Navigation.css';
 
-function navigation() {
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector(state => state.session.user);
+  let sessionLinks;
+  if (sessionUser) {
+    sessionLinks = (<ProfileButton user={sessionUser} />)
+  } else {
+    sessionLinks = (
+      <>
+        <NavLink to='/login'>Log In</NavLink>
+        <NavLink to='/signup'>Sign Up</NavLink>
+      </>)
+  }
 
   return (
     <ul>
-      {}
+      <li>
+        <NavLink exact to='/'>Home</NavLink>
+        { isLoaded && sessionLinks }
+      </li>
     </ul>
-  )
+  );
 }
-// show a button or link or something that onclick useEffect:
-// dispatch(sessionActions.logout)
+
+export default Navigation;
