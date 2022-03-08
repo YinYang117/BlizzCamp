218ddb7e1e7b8c2288350ed801f3e408aa7a1380
YinYang117 <slcryan2010@yahoo.com>
2022-03-08 14:59:24 -0700

    working on search bar

--- frontend/src/components/HomePage/HomePageIntro.js
+++ frontend/src/components/HomePage/HomePageIntro.js
@@ -3,7 +3,7 @@ import React from "react";
 function HomePageIntro() {
   return (
     <>
-      <h1 id='home-h1' >Find the ultimate getaway</h1>
+      <h1 className='home-h1' >Find the ultimate getaway</h1>
       <h2 className='home-h2'>New locations across the galaxy</h2>
       <h2 className='home-h2'>Just waiting for you</h2>
     </>
--- frontend/src/components/HomePage/SearchLoggedOut.js
+++ frontend/src/components/HomePage/SearchLoggedOut.js
@@ -6,9 +6,21 @@ function SearchLoggedOut() {
       <div className='search-container-logged-in'>
         <div className='world-or-location-query'>
           <p>Where To?</p>
-          <div id='where-query-area' className='white-background'>
+          <div className='white-background where-to'>
             <i className="fa-solid fa-magnifying-glass-location"></i>
-            <input id='query-input'> Where to input </input>
+            <input id="search-input" type="text" placeholder="World or Location..."></input>
+          </div>
+        </div>
+        <div className='date-query'>
+          <p>Dates</p>
+          <div className='white-background search-dates'>
+            <i class="fa-solid fa-calendar" />
+            <input className='search-input' type="text" placeholder="World or Location..."></input>
+          </div>
+        </div>
+        <div className='search-button-container'>
+          <div className='circle-background'>
+            <i className="fa-solid fa-magnifying-glass" />
           </div>
         </div>
       </div>
